fbd50fb9accdc21a12fbcca99c025c8d6fb0115a
ucihafafa-alt <ucihafafa@gmail.com>
2026-06-28 13:17:13 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE='tengerin-melmii-v21';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','app.js','icon.png','bg.jpg','manifest.json']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
